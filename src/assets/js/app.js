61e07e5c17fe78c7b1a682062415e4b7fa345b78
andrei_labadaev <andrei-labadaev@mail.ru>
2022-07-31 16:38:22 +0300

    uppfate

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -354,11 +354,17 @@ $(function () {
 		rows: 0,
 		responsive: [
 			{
-				breakpoint: 120,
+				breakpoint: 992,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1,
-					dots: false
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
 				}
 			}
 		]
